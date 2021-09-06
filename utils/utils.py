98b70d1cc12f7ccb505e50382a69bcbc221c8913
w.w <38289341+kartik4949@users.noreply.github.com>
2021-09-07 02:05:46 +0530

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,13 @@
+''' a simple utility functions. '''
+import os
 
+# Mappers
+_os_command_mapper = {'clear':{'windows':'cls', 'linux':'clear'}}
+
+def clear_screen(os_name='linux'):
+    try:
+        cmd = _os_command_mapper['clear'][os_name]
+        assert isinstance(cmd, str), 'command should be string.'
+        os.system(cmd)
+    except KeyError as ke:
+        raise KeyError(f'os {os_name} not found in os command mapper!!')
